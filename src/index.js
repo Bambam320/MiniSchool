1565e24025aeef6e06c7d22c1e11515a8edf627b
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-09-17 15:24:57 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
