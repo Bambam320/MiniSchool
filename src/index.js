00ce9055d807b1e5b990e2c71fec874d78718a39
Bambam320 <igor.mestechkin@gmail.com>
2022-09-21 15:42:54 -0400

    changing index file

--- src/index.js
+++ src/index.js
@@ -9,37 +9,36 @@ import Student from './components/Student';
 import Explore from './components/Explore';
 import Login from './components/Login';
 import Signup from './components/Signup';
-
 debugger
-const [state, setState] = useState('orange')
-return(
-<SomethingContext.Provider value={state, setState}/>
-  <Consumer //other components but they should require the context which has to imported>
-<SomethinContext.Provider/>
-)
-Consumer apps
-import { useContext } from 'react'
-import { SomethingContext } from 'SomethingContext location'
-const { state, setState } useContext(AppContent)
 
-const root = document.getElementById('root');
-ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="faculty" element={<Faculty />} />
-          <Route path="student" element={<Student />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </React.StrictMode>
-  </BrowserRouter>,
-  root
-);
+function index () {
+  const LoggedUserContext = createContext();
 
+  const [currentUser, setCurrentUser] = useState('')
+
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <Routes>
+          <LoggedUserContext.Provider value = {{currentUser, setCurrentUser}}>
+            <Route path="/" element={<App />}>
+              <Route index element={<Home />} />
+              <Route path="faculty" element={<Faculty />} />
+              <Route path="student" element={<Student />} />
+              <Route path="explore" element={<Explore />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+            </Route>
+          </LoggedUserContext.Provider>
+        </Routes>
+      </React.StrictMode>
+    </BrowserRouter>,
+    root
+  );
+
+}
+
+index()
 
 
