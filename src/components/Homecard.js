ee49cbfeff9455652a1a73af39ce43dfaf46ccc7
Bambam320 <igor.mestechkin@gmail.com>
2022-09-20 22:56:08 -0400

    new local verified

--- src/components/Homecard.js
+++ src/components/Homecard.js
@@ -16,7 +16,7 @@ function Homecard({card}) {
   let bookId = (Object.keys(card)[0])
   // let imageUrl = book[`${bookId}`] || 'https://media.istockphoto.com/vectors/loading-icon-vector-illustration-vector-id1335247217?k=20&m=1335247217&s=612x612&w=0&h=CQFY4NO0j2qc6kf4rTc0wTKYWL-9w5ldu-wF8D4oUBk='
   // let title = book[`${bookId}`]
-  console.log(card[`${bookId}`])
+  console.log(card[`${bookId}`]) //more stuff
   return(
     <Card sx={{ maxWidth: 345}}> title here
       <CardMedia
